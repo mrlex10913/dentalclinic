4ea8eb97c92cec93024cfb3744285666deeab25b
mrlex10913 <alexandrei10913@gmail.com>
2023-07-30 10:38:01 +0800

    My Second Commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\PostIndex;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/post', PostIndex::class)->name('posts.index');
